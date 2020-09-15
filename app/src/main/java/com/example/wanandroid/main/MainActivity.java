224c24141bcbfdbf99f809bc54cd6e580c51ca1b
zhaojianwei01 <3058587756@qq.com>
2020-09-15 16:59:14 +0800

    拍照

--- app/src/main/java/com/example/wanandroid/main/MainActivity.java
+++ app/src/main/java/com/example/wanandroid/main/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.wanandroid.main;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.internal.view.SupportMenuItem;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupWindow;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -30,18 +41,24 @@ import com.example.wanandroid.fragment.SystemFragment;
 import com.example.wanandroid.popupwindow.PhotoPopupWindow;
 import com.google.android.material.navigation.NavigationView;
 import com.hjq.bar.OnTitleBarListener;
-import com.hjq.bar.TitleBar;
 import com.hjq.toast.ToastUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
     private List<Fragment> fragmentList = new ArrayList<>();
     private BottomBarAdapter adapter;
     //databinding
     private ActivityMainBinding binding;
+    //点击头像处理事件
+    public static final int TAKE_PHOTO = 1;//声明一个请求码，用于识别返回的结果
+    private Uri imageUri;
+    private ImageView picture;
+    private final String filePath = Environment.getExternalStorageDirectory() + File.separator + "output_image.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +79,10 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private void initView() {
         binding.rg.setOnCheckedChangeListener(this);
         binding.rg.getChildAt(0).performClick();
+        //个人中心图片点击更换个人头像
+        picture = binding.mainNavigationView.getHeaderView(0).findViewById(R.id.iv_user);
+
+        setDefualtHead();
     }
 
     private void initViewPager() {
@@ -212,6 +233,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 switch (type){
                     case 0:  //拍照
                         Toast.makeText(getBaseContext(),"拍照",Toast.LENGTH_SHORT).show();
+                        requestPermission();
                         break;
                     case 1:  //相册
                         Toast.makeText(getBaseContext(),"相册",Toast.LENGTH_SHORT).show();
@@ -222,5 +244,89 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 }
             }
         });
+    }
+
+    /**
+     *  拍照实现
+     */
+    //动态请求权限
+    private void requestPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //请求权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+        } else {
+            //调用
+            requestCamera();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults != null && grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                case 1: {
+                    requestCamera();
+                }
+                break;
+            }
+        }
+    }
+    private void requestCamera() {
+        File outputImage = new File(filePath);
+        try
+        {
+            if (!outputImage.getParentFile().exists()) {
+                outputImage.getParentFile().mkdirs();
+            }
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+
+            outputImage.createNewFile();
+
+            if (Build.VERSION.SDK_INT >= 24) {
+                imageUri = FileProvider.getUriForFile(this,
+                        "com.example.wanandroid.fileprovider", outputImage);
+            } else {
+                imageUri = Uri.fromFile(outputImage);
+            }
+            //使用隐示的Intent，系统会找到与它对应的活动，即调用摄像头，并把它存储
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, TAKE_PHOTO);
+            //调用会返回结果的开启方式，返回成功的话，则把它显示出来
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //处理返回结果的函数，下面是隐示Intent的返回结果的处理方式
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                        //将图片解析成Bitmap对象，并把它显现出来
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setDefualtHead() {
+        File outputImage = new File(filePath);
+        if (!outputImage.exists()) {
+            return;
+        }
+        picture.setImageBitmap(BitmapFactory.decodeFile(filePath));
     }
 }

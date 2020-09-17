4496bacef42b50f327fc64f57e8f9f52c17802be
zhaojianwei01 <3058587756@qq.com>
2020-09-17 09:51:08 +0800

    相册功能补全

--- app/src/main/java/com/example/wanandroid/main/MainActivity.java
+++ app/src/main/java/com/example/wanandroid/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wanandroid.main;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -12,14 +13,18 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -55,15 +60,19 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     //databinding
     private ActivityMainBinding binding;
     //点击头像处理事件
+    //拍照
     public static final int TAKE_PHOTO = 1;//声明一个请求码，用于识别返回的结果
     private Uri imageUri;
     private ImageView picture;
     private final String filePath = Environment.getExternalStorageDirectory() + File.separator + "output_image.jpg";
+    //相册
+    public static final int CHOOSE_PHOTO = 2;
+    String imagePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         //初始化控件
         initView();
         //绑定RadioButton
@@ -76,13 +85,15 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         onNavigationView();
 
     }
+
     private void initView() {
         binding.rg.setOnCheckedChangeListener(this);
         binding.rg.getChildAt(0).performClick();
         //个人中心图片点击更换个人头像
         picture = binding.mainNavigationView.getHeaderView(0).findViewById(R.id.iv_user);
 
-        setDefualtHead();
+        //设置上次打开app时保存的图片
+        setKeepImage();
     }
 
     private void initViewPager() {
@@ -133,6 +144,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
             }
         });
     }
+
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
         switch (checkedId) {
@@ -156,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     }
 
     //自定义TitleBar的点击事件
-    private void onClickTitleBar(){
+    private void onClickTitleBar() {
         binding.titleBar.setOnTitleBarListener(new OnTitleBarListener() {
             @SuppressLint("WrongConstant")
             @Override
@@ -182,9 +194,9 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
 
     //侧滑栏的事件处理
-    private void onNavigationView(){
+    private void onNavigationView() {
         //头像点击事件处理
-        binding.mainNavigationView.getHeaderView(0).findViewById(R.id.iv_user).setOnClickListener((View)->{
+        binding.mainNavigationView.getHeaderView(0).findViewById(R.id.iv_user).setOnClickListener((View) -> {
             //点击头像弹出PopupWindow
             showPopupWindow(binding.mainLayout);
         });
@@ -192,26 +204,26 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         binding.mainNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.integral :
+                switch (item.getItemId()) {
+                    case R.id.integral:
                         ToastUtils.show("积分");
                         break;
-                    case R.id.share :
+                    case R.id.share:
                         ToastUtils.show("分享");
                         break;
-                    case R.id.collect :
+                    case R.id.collect:
                         ToastUtils.show("收藏");
                         break;
-                    case R.id.todo :
+                    case R.id.todo:
                         ToastUtils.show("TODO");
                         break;
-                    case R.id.setting :
+                    case R.id.setting:
                         ToastUtils.show("设置");
                         break;
-                    case R.id.about :
+                    case R.id.about:
                         ToastUtils.show("关于我们");
                         break;
-                    case R.id.esc :
+                    case R.id.esc:
                         ToastUtils.show("退出登录");
                         finish();
                         break;
@@ -222,24 +234,23 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     }
 
     //点击头像进行相册/拍照选择
-    private void showPopupWindow(View parent){
+    private void showPopupWindow(View parent) {
         PhotoPopupWindow popupWindow = new PhotoPopupWindow(this);
         //定义PopupWindow的弹出位置
-        popupWindow.showAtLocation(parent, Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);//在activity的底部展示。
+        popupWindow.showAtLocation(parent, Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);//在activity的底部展示。
 //            popupWindow.showAsDropDown(binding.showPopupWindow); //在某个控件下方弹出Popupwindow
         popupWindow.setImageClickaListener(new PhotoPopupWindow.PhotoDataListener() {
             @Override
             public void getTestData(int type) {
-                switch (type){
+                switch (type) {
                     case 0:  //拍照
-                        Toast.makeText(getBaseContext(),"拍照",Toast.LENGTH_SHORT).show();
-                        requestPermission();
+                        requestPermission(0);
                         break;
                     case 1:  //相册
-                        Toast.makeText(getBaseContext(),"相册",Toast.LENGTH_SHORT).show();
+                        requestPermission(1);
                         break;
                     case 2:  //取消
-                        Toast.makeText(getBaseContext(),"取消",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "取消", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
@@ -247,19 +258,24 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     }
 
     /**
-     *  拍照实现
+     * 动态请求权限
      */
-    //动态请求权限
-    private void requestPermission() {
+
+    private void requestPermission(int type) {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             //请求权限
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
         } else {
-            //调用
-            requestCamera();
+            //根据点击的类型调用特性的方法。
+            if (type == 0) {
+                requestCamera();
+            } else {
+                openAlbum();
+            }
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -269,15 +285,21 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                     requestCamera();
                 }
                 break;
+                case 2: {
+                    openAlbum();
+                }
+                break;
             }
         }
     }
 
+    /**
+     * 调用摄像头拍照功能
+     */
     //请求相机拍照
     private void requestCamera() {
         File outputImage = new File(filePath);
-        try
-        {
+        try {
             if (!outputImage.getParentFile().exists()) {
                 outputImage.getParentFile().mkdirs();
             }
@@ -290,8 +312,6 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
             if (Build.VERSION.SDK_INT >= 24) {
                 imageUri = FileProvider.getUriForFile(this,
                         "com.example.wanandroid.fileprovider", outputImage);
-            } else {
-                imageUri = Uri.fromFile(outputImage);
             }
             //使用隐示的Intent，系统会找到与它对应的活动，即调用摄像头，并把它存储
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
@@ -301,7 +321,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        displayImage(filePath);
     }
 
     //处理返回结果的函数，下面是隐示Intent的返回结果的处理方式
@@ -319,17 +339,94 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    }
+                }
+                break;
             default:
                 break;
         }
     }
 
-    //设置保存拍照图片——>再次关闭app重新打开为上次拍照照片
-    private void setDefualtHead() {
-        File outputImage = new File(filePath);
-        if (!outputImage.exists()) {
-            return;
+    /**
+     * 从相册选择照片功能
+     */
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO); //打开相册
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitKat(Intent data) {
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];  //解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
         }
-        picture.setImageBitmap(BitmapFactory.decodeFile(filePath));
+        displayImage(imagePath); //根据图片路径显示图片
+    }
+
+    //将选择的图片Uri转换为路径
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    /**
+     * 展示图片 (公用)
+     *
+     * @param imagePath
+     */
+    private void displayImage(String imagePath) {
+        if (imagePath != null && !imagePath.equals("")) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+            //存储上次选择的图片路径，用以再次打开app设置图片
+            SharedPreferences sp = getSharedPreferences("sp_img", MODE_PRIVATE);  //创建xml文件存储数据，name:创建的xml文件名
+            SharedPreferences.Editor editor = sp.edit(); //获取edit()
+            editor.putString("imgPath", imagePath);
+            editor.apply();
+        } else {
+            Toast.makeText(this, "获取图片失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 设置上一次打开app时保存图片，为当前activity显示用户头像
+     **/
+    private void setKeepImage() {
+        SharedPreferences sp = getSharedPreferences("sp_img", MODE_PRIVATE);
+        //取出上次存储的图片路径设置此次的图片展示
+        String beforeImagePath = sp.getString("imgPath", null);
+        displayImage(beforeImagePath);
     }
 }
